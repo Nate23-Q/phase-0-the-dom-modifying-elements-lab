f14496aadf47f6ec077fbc3121e9b50db5128a8f
Nate23-Q <nathan.isaac@student.moringaschool.com>
2025-06-27 19:02:42 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+// Remove the <main id="main"> element
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Dallas is the champion';
+
+document.body.appendChild(newHeader);// Remove the <main id="main"> element
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Dallas is the champion';
+
+document.body.appendChild(newHeader);
